001afc3c1473c6085e3f72515bd6c409cd0ae372
Mohammad Riyaz Aly <riyazmohammadrj786@gmail.com>
2023-03-17 22:50:31 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -3,33 +3,48 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');
   const [darkOrLi, setDarkOrLi] = useState('dark');
   const [btntext, setBtnText] = useState('Switch DarkMode');
+  const [alert, setAlert] = useState(null);
 
+  const showAlert = (message, type) =>{
+      setAlert({
+        msg : message,
+        type : type
+      })
+      setTimeout(() =>{
+        setAlert(null);
+      },1500);
+  }
 
   const EnableToggle = ()=>{
-    if(mode == 'light'){
+    if(mode === 'light'){
       setMode('dark');
       setDarkOrLi('light');
       setBtnText('Switch to LightMode');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been enabled", "success");
     }else{
       setMode('light');
       setDarkOrLi('dark');
       setBtnText('Switch to DarkMode');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
 
   return (
     <>
-
       {/* //u can mention data here */}
       {/* <Navbar/> */}
       <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggle={EnableToggle} dol = {darkOrLi} bt = {btntext}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the Text to analyze"/>
+        <TextForm showAlert = {showAlert} heading="Enter the Text to analyze" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,28 +12,37 @@ export default function TextForm(props) {
         // console.log("Uppercase clicked:" + text);
         const newText = text.toUpperCase();
         setText(newText);
+        // props.showAlert("Converted to UpperCase", "success");
+        text.length > 0 ? props.showAlert("Converted to UpperCase", "success") : props.showAlert("Enter some text to Convert", "danger");
     }
 
     const hanldeLoClick = ()=>{
         // console.log("Uppercase clicked:" + text);
         const newText = text.toLowerCase();
         setText(newText);
+        // props.showAlert("Converted to LowerCase", "success");
+        text.length > 0 ? props.showAlert("Converted to LowerCase", "success") : props.showAlert("Enter some text to Convert", "danger");
+
     }
 
     const hanldeClearClick = ()=>{
         const newText = '';
         setText(newText);
+        props.showAlert("Text is Cleared", "success");
     }
 
     const handleCopy = ()=>{
         var text = document.getElementById("myText");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Text is Copied to Clipboard", "success");
     }
 
     const handleExtraSpace = ()=>{
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "));
+        setText(newText.join(" ")); 
+        text.length > 0 ? props.showAlert("Extra Space has been removed", "success") : props.showAlert("Enter some text to remove extra Space", "danger");
+
     }
 
     //on change should write because the event should listened what u are typing
@@ -45,10 +54,10 @@ export default function TextForm(props) {
     
     return (
         <>
-        <div className="container">
+        <div className="container" style = {{color: props.mode === 'dark' ? 'white' : 'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myText" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style = {{backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black'} } id="myText" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={hanldeUpClick}>Convert to upperCase</button>
             <button className="btn btn-primary mx-2 my-2" onClick={hanldeLoClick}>Convert to LowerCase</button>
@@ -57,12 +66,12 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpace}>Remove Extra Spaces</button>
         </div>
 
-        <div className="container my-2">
+        <div className="container my-2" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes to read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : "Enter some text to preview here"}</p>
         </div>
         </>
     )
